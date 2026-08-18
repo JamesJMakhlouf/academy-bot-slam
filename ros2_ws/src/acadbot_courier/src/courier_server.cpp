bce120c55c993ff4294036032b40aa944a014154
JamesJMakhlouf <jamesjmakhlouf@gmail.com>
2026-08-18 13:34:54 +0300

    Add job acceptance service with validation and job IDs

--- ros2_ws/src/acadbot_courier/src/courier_server.cpp
+++ ros2_ws/src/acadbot_courier/src/courier_server.cpp
@@ -1,7 +1,14 @@
 #include "rclcpp/rclcpp.hpp"
+#include "acadbot_courier_interfaces/srv/accept_job.hpp"
+
+using AcceptJob = acadbot_courier_interfaces::srv::AcceptJob;
 
 struct Location {
     double x, y, yaw;
+};
+
+struct Job {
+    std::string pickup, dropoff;
 };
 
 class CourierServer : public rclcpp::Node {
@@ -19,6 +26,9 @@ class CourierServer : public rclcpp::Node {
             for (size_t i = 0; i < names.size() && 3  * (i + 1) <= poses.size(); ++i) {
                 locations_[names[i]] = {poses[3 * i], poses[3 * i + 1], poses[3 * i + 2]};
             }
+
+            accept_service_ = create_service<AcceptJob>("courier/accept_job", std::bind(&CourierServer::handle_accept_job, this, std::placeholders::_1, std::placeholders::_2));
+
             RCLCPP_INFO(get_logger(), "Courier server initialized with %zu locations, %d retries", locations_.size(), retry_count_);
             for (const auto& [name, loc] : locations_) {
                 RCLCPP_INFO(get_logger(), "Location %s: x=%.2f, y=%.2f, yaw=%.2f", name.c_str(), loc.x, loc.y, loc.yaw);
@@ -33,6 +43,48 @@ class CourierServer : public rclcpp::Node {
         double leg_timeout_sec_;
         bool busy_reject_;
         std::unordered_map<std::string, Location> locations_;
+
+        rclcpp::Service<AcceptJob>::SharedPtr accept_service_;
+        std::unordered_map<uint32_t, Job> jobs_;
+        uint32_t next_job_id_{1};
+        bool job_active_{false};
+        uint32_t active_job_id_{0};
+
+        void handle_accept_job(const std::shared_ptr<AcceptJob::Request> request, std::shared_ptr<AcceptJob::Response> response) {
+            auto pickup = locations_.find(request->pickup_name);
+            auto dropoff = locations_.find(request->dropoff_name);
+
+            if (pickup == locations_.end()) {
+                response->accepted = false;
+                response->reason = "unknown location '" + request->pickup_name + "'";
+                return;
+            }
+
+            if (dropoff == locations_.end()) {
+                response->accepted = false;
+                response->reason = "unknown location '" + request->dropoff_name + "'";
+                return;
+            }
+
+            if (request->pickup_name == request->dropoff_name) {
+                response->accepted = false;
+                response->reason = "pickup and dropoff locations are the same";
+                return;
+            }
+
+            if (busy_reject_ && job_active_) {
+                response->accepted = false;
+                response->reason = "robot is busy with job " + std::to_string(active_job_id_);
+                return;
+            }
+
+            const uint32_t id = next_job_id_++;
+            jobs_[id] = {request->pickup_name, request->dropoff_name};
+            response->accepted = true;
+            response->job_id = id;
+
+            RCLCPP_INFO(get_logger(), "Accepted job %u: %s -> %s", id, request->pickup_name.c_str(), request->dropoff_name.c_str());
+        }
 };
 
 int main(int argc, char** argv) {
